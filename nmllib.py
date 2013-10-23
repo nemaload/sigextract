dccd18d177d27013b9a57bc651a9629560d10049
Petr Baudis <pasky@ucw.cz>
2013-10-24 00:08:23 +0200

    nmllib: Print progress on stderr

--- nmllib.py
+++ nmllib.py
@@ -1,19 +1,22 @@
 # NeuroML2 tools library
 
+from __future__ import print_function
+
 import glob
 import numpy
+import sys
 
 import neuroml
 import neuroml.loaders as loaders
 
 def load_neuron(filename):
-    print "Loading " + filename + "..."
+    print("Loading " + filename + "...", file = sys.stderr)
     doc = loaders.NeuroMLLoader.load(filename)
     neurons = []
     for cell in doc.cells:
         soma_segid = filter(lambda g: g.id == "Soma", cell.morphology.segment_groups)[0].members[0].segments
         segment = cell.morphology.segments[soma_segid]
-        print "  Loading cell " + cell.id
+        print("  Loading cell " + cell.id, file = sys.stderr)
         # Normally, proximal and distal coordinates will be the same in our
         # dataset; if not, average them just to be sure; then consider a circle
         # around this coordinate to be the neuron location.
